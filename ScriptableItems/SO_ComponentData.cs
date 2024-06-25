722d60b04bcd6e22c8c1cebecee82a95ee933fd3
Lomias <460492314@qq.com>
2024-06-25 17:47:22 +0800

    update data

---
+++ ScriptableItems/SO_ComponentData.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="Component",menuName ="Data/Component",order =0)]
+public class SO_ComponentData : ScriptableObject
+{
+    public string ComponentID;
+    public string ComponentName;
+    public float ComponentEndurance;
+    public float ComponentInternalBattery;
+    public bool isFatalComponent;
+    public CompFunctionDetail[] functions;
+}
+[Serializable]
+public struct CompFunctionDetail
+{
+    public string functionName;
+    public BaseComponent.ComponentFunction function;
+    public float functionValue;
+    public float functionConsume;
+    public int[] functionIntVal;
+    public float[] functionFloatVal;
+    public bool[] functionBoolVal;
+    public string[] functionStringVal;
+}
